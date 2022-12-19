2b43396a3b2c43ef71b8e5431f102a02ff06c4b2
omp12345 <opsgp1999@gmail.com>
2022-12-19 09:43:08 +0530

    ok

---
+++ nav.js
@@ -0,0 +1,16 @@
+let care=document.getElementById("care")
+let signup=document.getElementById("image")
+let get=document.getElementById("get")
+
+signup.addEventListener("click",()=>{
+window.location.assign("./signup.html")
+
+
+
+})
+let img=document.getElementById("img")
+img.addEventListener("click",()=>{
+   window.location.assign("./nav.html")
+})
+
+
